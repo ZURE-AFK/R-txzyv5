b5348ea44fdda1d69f386415447c002746b69d40
Verdi hatoru <95448883+verdihatorou@users.noreply.github.com>
2022-12-18 14:46:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ loadDatabase()
 // if (opts['cluster']) {
 //   require('./lib/cluster').Cluster()
 // }
-global.authFile = `${opts._[0] || 'R-TXZY'}.data.json`
+global.authFile = `${opts._[0] || 'R-txzyv5'}.data.json`
 global.isInit = !fs.existsSync(authFile)
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
